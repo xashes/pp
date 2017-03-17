c87ba2c4ee57126b97f4d659e759ad15256903a7
xashes <xashes@outlook.com>
2017-03-18 00:32:26 +0800

    add package line

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 package = 'pp'
 version = '0.1'
 
-setup(name=package,
-      version=version,
-      description="Poker Hands Evaluation",
-      url='url')
+setup(
+    name=package,
+    version=version,
+    description="Poker Hands Evaluation",
+    url='https://github.com/xashes/pp',
+    package=find_packages(exclude=('tests')))
